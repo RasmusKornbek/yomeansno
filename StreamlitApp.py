1179855a44e6ffcccaae8fbfb9891f37df1b4bea
RasmusKornbek <35880535+RasmusKornbek@users.noreply.github.com>
2023-09-06 17:00:35 +0200

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -7,3 +7,18 @@ streamlit.title('Zena\'s Amazing Athleisure Catalog')
 # connect to snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
+
+
+# streamlit_app.py
+
+import streamlit as st
+
+# Initialize connection.
+conn = st.experimental_connection('snowpark')
+
+# Perform query.
+df = conn.query('SELECT * from mytable;', ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.NAME} has a :{row.PET}:")
