9f2b1001ebe8c4e9b42ecdecc8548e134d49a71d
RasmusKornbek <35880535+RasmusKornbek@users.noreply.github.com>
2023-09-07 09:25:39 +0200

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -9,12 +9,4 @@ st.set_page_config(layout="wide")
 # Get current session
 session = get_active_session()
 
-@st.cache_data()
-def load_data():
-    # Load CO2 emissions data
-    snow_df_co2 = session.table("streamlit.dsi.mytable").filter(col('NAME') == 'Mary').filter(col('PET') == 'All Type').sort('"Date"').with_column_renamed('"Date"','"Year"')
 
-    return snow_df_co2.to_pandas()
-
-# Load and cache data
-df_co2_overtime = load_data()
